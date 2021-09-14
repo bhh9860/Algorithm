fdf79a6eaed0172ce0358345eec9bca6c41161cd
Jeong Hyeon Tang <34750753+bhh9860@users.noreply.github.com>
2021-09-14 20:04:10 +0900

    소요시간 25분 실버4

---
+++ Python/2021/210914-10866.py
@@ -0,0 +1,30 @@
+import sys
+from collections import deque
+lists = deque(list())
+input = sys.stdin.readline
+
+for _ in range(int(input())):
+	a, *b = list(map(str, input().split()))
+	if b:
+		if a[-1] == 't':
+			lists.appendleft(b[0])
+		else:
+			lists.append(b[0])
+	
+	elif a == 'pop_front':
+		print(lists.popleft()) if lists else print(-1)
+	
+	elif a == 'pop_back':
+		print(lists.pop()) if lists else print(-1)
+	
+	elif a == 'size':
+		print(len(lists))
+		
+	elif a == 'empty':
+		print(1) if not lists else print(0)
+	
+	elif a == 'front':
+		print(lists[0]) if lists else print(-1)
+		
+	elif a == 'back':
+		print(lists[-1]) if lists else print(-1)
