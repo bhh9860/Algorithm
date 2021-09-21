3448fbe19df16f36bb6f25f119afcaa0d37556b4
Jeong Hyeon Tang <34750753+bhh9860@users.noreply.github.com>
2021-09-21 20:42:14 +0900

    소요시간 55분 실버5 브루트포스 알고리즘 포기 내일 함...

---
+++ Python/2021/210921-1436.py
@@ -0,0 +1,13 @@
+n = int(input())
+lists = ['0666', '1666', '2666', '3666', '4666', '5666', '6660', '6661', '6662', '6663', '6664', '6665', '6666', '6667', '6668', '6669', '7666', '8666', '9666']
+
+namuji = n % 19 - 1
+mok = (n-1) // 19
+
+if mok > 0:
+	print(f'{mok}{lists[namuji]}')
+else:
+	if namuji == 0:
+		print(f'{lists[namuji][1:4]}')
+	else:
+		print(f'{lists[namuji]}')
